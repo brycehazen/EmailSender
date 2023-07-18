4dd8c257296543530b0fae5c72353f544bdf0eb2
brycehazen <39276420+brycehazen@users.noreply.github.com>
2023-07-18 13:30:47 -0400

    Update EmailDataSender.py
    A Flask application that serves HTML pages and handles file uploads.

--- EmailDataSender.py
+++ EmailDataSender.py
@@ -1,42 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
-import csv
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pandas as pd
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
 
 app = Flask(__name__)
+app.secret_key = 'your secret key'  # for flash messaging
 
-# Home route - displays the form
+# Define your email credentials
+smtp_server = 'smtp-mail.outlook.com'
+smtp_port = 587
+email_user = 'youremail@outlook.com'
+email_pass = 'yourpassword'
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Send emails route - handles the form submission and sends the emails
-@app.route('/send_emails', methods=['POST'])
-def send_emails():
-    # Get form data
-    csv_file = request.files['csv_file']
-    email = request.form['email']
-    password = request.form['password']
-    email_body = request.form['email_body']
+@app.route('/preview', methods=['POST'])
+def preview():
+    if 'csvfile' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['csvfile']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    df = pd.read_csv(file)
+    email_template = request.form['email_template']
+    preview_email = email_template.format(name=df.loc[0, 'name'], donation_amount=df.loc[0, 'donation_amount'])
+    return render_template('preview.html', preview_email=preview_email, emails=df['email'].tolist())
 
-    # Read the CSV file
-    csv_data = csv_file.read().decode('utf-8').splitlines()
-    csv_reader = csv.reader(csv_data)
-    next(csv_reader)  # Skip the header row
-
-    # Send emails
-    for row in csv_reader:
-        email_address = row[0]
-        subject = row[1]
-        body = row[2]
-
-        # Send the email using smtplib
-        message = f"Subject: {subject}\n\n{email_body}"
-        with smtplib.SMTP('smtp.gmail.com', 587) as server:
-            server.starttls()
-            server.login(email, password)
-            server.sendmail(email, email_address, message)
-
-    return 'Emails sent successfully!'
+@app.route('/send', methods=['POST'])
+def send():
+    file = request.files['csvfile']
+    df = pd.read_csv(file)
+    email_template = request.form['email_template']
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.login(email_user, email_pass)
+    for index, row in df.iterrows():
+        msg = MIMEMultipart()
+        msg['From'] = email_user
+        msg['To'] = row['email']
+        msg['Subject'] = 'Fundraiser update'
+        body = email_template.format(name=row['name'], donation_amount=row['donation_amount'])
+        msg.attach(MIMEText(body, 'plain'))
+        text = msg.as_string()
+        server.sendmail(email_user, row['email'], text)
+    server.quit()
+    flash('Emails sent successfully.')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
